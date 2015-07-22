556d2a01f7e6e3997e296f666a2e79723d363aa2
Ludovic Fauvet <etix@videolan.org>
2015-07-22 18:15:20 +0200

    Build script for cross-compiling libvlc
    - Setup the environment
    - Checkout vlc.git (if needed)
    - Ensures vlc.git is at least to a given revision
    - Download and build the contribs
    - And finally build libvlc

---
+++ buildvlc.sh
@@ -0,0 +1,578 @@
+#!/bin/bash
+
+#############
+#  DEFINES  #
+#############
+
+VLC_TESTED_HASH=044730b
+GCCVER=4.8
+GCCVERFULL=4.8.3
+
+#############
+# FUNCTIONS #
+#############
+
+checkfail()
+{
+    if [ ! $? -eq 0 ];then
+        echo -e "\e[1m\e[31m$1\e[0m"
+        exit 1
+    fi
+}
+
+#############
+# ARGUMENTS #
+#############
+
+SCRIPT=$(readlink -f "$0")
+PROJECTPATH=$(dirname "$SCRIPT")
+
+RELEASE=0
+while [ $# -gt 0 ]; do
+    case $1 in
+        help|--help)
+            echo "Use -a to set the ARCH"
+            echo "Use --release to build in release mode"
+            exit 1
+            ;;
+        a|-a)
+            TIZEN_ABI=$2
+            shift
+            ;;
+        release|--release)
+            RELEASE=1
+            ;;
+    esac
+    shift
+done
+
+if [ -z "$TIZEN_SDK" ]; then
+    echo "Please set the TIZEN_SDK environment variable with its path."
+    exit 1
+fi
+
+if [ -z "$TIZEN_ABI" ]; then
+    echo "Please pass the TIZEN_ABI to the correct architecture, using
+                ${SCRIPT} -a ARCH
+    ARM:     armv7l
+    X86:     x86_64"
+    exit 1
+fi
+
+#####################
+# FETCH VLC SOURCES #
+#####################
+
+if [ ! -d "${PROJECTPATH}/vlc" ]; then
+    echo -e "\e[1m\e[32mVLC source not found, cloning\e[0m"
+    git clone git://git.videolan.org/vlc.git "${PROJECTPATH}/vlc"
+    checkfail "vlc source: git clone failed"
+else
+    echo -e "\e[1m\e[32mVLC source found\e[0m"
+    cd "${PROJECTPATH}/vlc"
+    if ! git cat-file -e ${VLC_TESTED_HASH}; then
+        cat << EOF
+***
+*** Error: Your vlc checkout does not contain the latest tested commit: ${VLC_TESTED_HASH}
+***
+EOF
+        exit 1
+    fi
+    cd ..
+fi
+
+###########################
+# VLC BOOTSTRAP ARGUMENTS #
+###########################
+
+VLC_BOOTSTRAP_ARGS="\
+    --disable-disc \
+    --disable-sout \
+    --enable-dvdread \
+    --enable-dvdnav \
+    --disable-dca \
+    --disable-goom \
+    --disable-chromaprint \
+    --disable-lua \
+    --disable-schroedinger \
+    --disable-sdl \
+    --disable-SDL_image \
+    --disable-fontconfig \
+    --enable-zvbi \
+    --disable-kate \
+    --disable-caca \
+    --disable-gettext \
+    --disable-mpcdec \
+    --enable-upnp \
+    --disable-gme \
+    --disable-tremor \
+    --enable-vorbis \
+    --disable-sidplay2 \
+    --disable-samplerate \
+    --disable-faad2 \
+    --disable-harfbuzz \
+    --enable-iconv \
+    --disable-aribb24 \
+    --disable-aribb25 \
+    --disable-mpg123 \
+    --disable-libdsm \
+    --enable-libarchive \
+    --enable-libtasn1 \
+"
+
+###########################
+# VLC CONFIGURE ARGUMENTS #
+###########################
+
+VLC_CONFIGURE_ARGS="\
+    --disable-nls \
+    --enable-live555 --enable-realrtsp \
+    --enable-avformat \
+    --enable-swscale \
+    --enable-avcodec \
+    --enable-opus \
+    --enable-opensles \
+    --enable-mkv \
+    --enable-taglib \
+    --enable-dvbpsi \
+    --disable-vlc \
+    --disable-shared \
+    --disable-update-check \
+    --disable-vlm \
+    --disable-dbus \
+    --disable-lua \
+    --disable-vcd \
+    --disable-v4l2 \
+    --disable-gnomevfs \
+    --enable-dvdread \
+    --enable-dvdnav \
+    --disable-bluray \
+    --disable-linsys \
+    --disable-decklink \
+    --disable-libva \
+    --disable-dv1394 \
+    --enable-mod \
+    --disable-sid \
+    --disable-gme \
+    --disable-tremor \
+    --enable-mad \
+    --disable-dca \
+    --disable-sdl-image \
+    --enable-zvbi \
+    --disable-fluidsynth \
+    --disable-jack \
+    --disable-pulse \
+    --disable-alsa \
+    --disable-samplerate \
+    --disable-sdl \
+    --disable-xcb \
+    --disable-atmo \
+    --disable-qt \
+    --disable-skins2 \
+    --disable-mtp \
+    --disable-notify \
+    --enable-libass \
+    --disable-svg \
+    --disable-udev \
+    --enable-libxml2 \
+    --disable-caca \
+    --disable-glx \
+    --disable-egl \
+    --disable-gles2 \
+    --disable-goom \
+    --disable-projectm \
+    --disable-sout \
+    --enable-vorbis \
+    --disable-faad \
+    --disable-x264 \
+    --disable-schroedinger \
+"
+
+########################
+# VLC MODULE BLACKLIST #
+########################
+
+VLC_MODULE_BLACKLIST="
+    addons.*
+    stats
+    access_(bd|shm|imem)
+    oldrc
+    real
+    hotkeys
+    gestures
+    sap
+    dynamicoverlay
+    rss
+    ball
+    audiobargraph_[av]
+    clone
+    mosaic
+    osdmenu
+    puzzle
+    mediadirs
+    t140
+    ripple
+    motion
+    sharpen
+    grain
+    posterize
+    mirror
+    wall
+    scene
+    blendbench
+    psychedelic
+    alphamask
+    netsync
+    audioscrobbler
+    motiondetect
+    motionblur
+    export
+    smf
+    podcast
+    bluescreen
+    erase
+    stream_filter_record
+    speex_resampler
+    remoteosd
+    magnify
+    gradient
+    dtstofloat32
+    logger
+    visual
+    fb
+    aout_file
+    yuv
+    .dummy
+"
+
+#########
+# FLAGS #
+#########
+
+# Set up ABI variables
+if [ "${TIZEN_ABI}" = "x86" ] ; then
+    TARGET_TUPLE="i386-linux-gnueabi"
+    PATH_HOST="x86"
+    PLATFORM_SHORT_ARCH="x86"
+elif [ "${TIZEN_ABI}" = "armv7l" ] ; then
+    TARGET_TUPLE="arm-linux-gnueabi"
+    PATH_HOST=$TARGET_TUPLE
+    HAVE_ARM=1
+    PLATFORM_SHORT_ARCH="arm"
+else
+    echo -e "\e[1m\e[31mUnknown ABI: '${TIZEN_ABI}'\e[0m"
+    exit 2
+fi
+
+SYSROOT=${TIZEN_SDK}/tools/${TARGET_TUPLE}-gcc-${GCCVER}
+LIBEXEC=${TIZEN_SDK}/tools/${TARGET_TUPLE}-gcc-${GCCVER}/libexec/gcc/${TARGET_TUPLE}/${GCCVERFULL}
+TIZEN_TOOLCHAIN_PATH=${SYSROOT}/bin
+
+export PATH=${TIZEN_TOOLCHAIN_PATH}:${LIBEXEC}:${PATH}
+
+
+###############
+# DISPLAY ABI #
+###############
+echo -e "\e[1m\e[36mABI:        $TIZEN_ABI\e[0m"
+echo -e "\e[1m\e[36mTIZEN SDK:  $TIZEN_SDK\e[0m"
+echo -e "\e[1m\e[36mSYSROOT:    $SYSROOT\e[0m"
+echo -e "\e[1m\e[36mPATH:       $PATH\e[0m"
+
+# Make in //
+if [ -z "$MAKEFLAGS" ]; then
+    UNAMES=$(uname -s)
+    MAKEFLAGS=
+    if which nproc >/dev/null; then
+        MAKEFLAGS=-j`nproc`
+    elif [ "$UNAMES" == "Darwin" ] && which sysctl >/dev/null; then
+        MAKEFLAGS=-j`sysctl -n machdep.cpu.thread_count`
+    fi
+fi
+
+##########
+# CFLAGS #
+##########
+
+CFLAGS="${CFLAGS} -fstrict-aliasing -funsafe-math-optimizations"
+
+EXTRA_CFLAGS="-fPIC"
+# Setup CFLAGS per ABI
+if [ "${TIZEN_ABI}" = "armv7l" ] ; then
+    EXTRA_CFLAGS="${EXTRA_CFLAGS} -march=armv7-a -mtune=cortex-a8 -mfpu=vfpv3-d16 -mthumb -mfloat-abi=softfp"
+elif [ "${TIZEN_ABI}" = "x86" ] ; then
+    EXTRA_CFLAGS="${EXTRA_CFLAGS} -mtune=atom -msse3 -mfpmath=sse -m32"
+fi
+
+EXTRA_CFLAGS="${EXTRA_CFLAGS} -I${SYSROOT}/include/c++/${GCCVERFULL}"
+EXTRA_CFLAGS="${EXTRA_CFLAGS} -I${SYSROOT}/include/c++/${GCCVERFULL}/${TARGET_TUPLE}"
+EXTRA_CFLAGS="${EXTRA_CFLAGS} -I${TIZEN_SDK}/platforms/mobile-2.3/rootstraps/mobile-2.3-device.core/usr/include"
+
+EXTRA_CXXFLAGS="-D__STDC_FORMAT_MACROS=1 -D__STDC_CONSTANT_MACROS=1 -D__STDC_LIMIT_MACROS=1"
+
+CPPFLAGS="-I${SYSROOT}/include/c++/${GCCVERFULL} -I${SYSROOT}/include/c++/${GCCVERFULL}/${TARGET_TUPLE} -I${TIZEN_SDK}/platforms/mobile-2.3/rootstraps/mobile-2.3-device.core/usr/include"
+
+#################
+# Setup LDFLAGS #
+#################
+
+TIZEN_TOOLS=${TIZEN_SDK}/tools/arm-linux-gnueabi-gcc-${GCCVER}/bin/arm-linux-gnueabi-
+CROSS_COMPILE=${TIZEN_TOOLS}
+TIZEN_FLAGS="--sysroot=${TIZEN_SDK}/platforms/mobile-2.3/rootstraps/mobile-2.3-device.core"
+export CC="${TIZEN_TOOLS}gcc ${TIZEN_FLAGS}"
+export CXX="${TIZEN_TOOLS}g++ ${TIZEN_FLAGS}"
+export AR="${TIZEN_TOOLS}ar"
+export LD="${TIZEN_TOOLS}ld ${TIZEN_FLAGS}"
+
+EXTRA_LDFLAGS="-L${TIZEN_SDK}/platforms/mobile-2.3/rootstraps/mobile-2.3-device.core/usr/lib"
+
+LDFLAGS="${TIZEN_FLAGS} -Wl,-Bdynamic -Wl,--no-undefined -lrt"
+
+if [ -n "$HAVE_ARM" ]; then
+        EXTRA_PARAMS=" --enable-neon"
+        LDFLAGS="${LDFLAGS} -Wl,--fix-cortex-a8"
+fi
+
+LDFLAGS="${LDFLAGS} -L${SYSROOT}/lib"
+
+# Release or not?
+if [ "$RELEASE" = 1 ]; then
+    OPTS=""
+    EXTRA_CFLAGS="${EXTRA_CFLAGS} -DNDEBUG "
+else
+    OPTS="--enable-debug"
+fi
+
+echo -e "\e[1m\e[36mCFLAGS:            ${CFLAGS}\e[0m"
+echo -e "\e[1m\e[36mEXTRA_CFLAGS:      ${EXTRA_CFLAGS}\e[0m"
+
+cd ${PROJECTPATH}/vlc
+
+###########################
+# Build buildsystem tools #
+###########################
+
+export PATH=${PROJECTPATH}/vlc/extras/tools/build/bin:$PATH
+echo -e "\e[1m\e[32mBuilding tools\e[0m"
+cd extras/tools
+./bootstrap
+checkfail "buildsystem tools: bootstrap failed"
+make $MAKEFLAGS
+checkfail "buildsystem tools: make"
+cd ../..
+
+#############
+# BOOTSTRAP #
+#############
+
+if [ ! -f configure ]; then
+    echo -e "\e[1m\e[32mBootstraping\e[0m"
+    ./bootstrap
+    checkfail "vlc: bootstrap failed"
+fi
+
+############
+# Contribs #
+############
+
+echo -e "\e[1m\e[32mBuilding the contribs\e[0m"
+mkdir -p contrib/contrib-tizen-${TARGET_TUPLE}
+
+gen_pc_file() {
+    echo "Generating $1 pkg-config file"
+    echo "Name: $1
+Description: $1
+Version: $2
+Libs: -l$1
+Cflags:" > contrib/${TARGET_TUPLE}/lib/pkgconfig/`echo $1|tr 'A-Z' 'a-z'`.pc
+}
+
+mkdir -p contrib/${TARGET_TUPLE}/lib/pkgconfig
+gen_pc_file EGL 1.1
+gen_pc_file GLESv2 2
+
+cd contrib/contrib-tizen-${TARGET_TUPLE}
+
+TIZEN_ABI=${TIZEN_ABI} TIZEN_API=${TIZEN_API} \
+    ../bootstrap --host=${TARGET_TUPLE} --build=x86_64-linux-gnu ${VLC_BOOTSTRAP_ARGS}
+checkfail "contribs: bootstrap failed"
+
+# TODO: mpeg2, theora
+
+# Some libraries have arm assembly which won't build in thumb mode
+# We append -marm to the CFLAGS of these libs to disable thumb mode
+[ ${TIZEN_ABI} = "armv7l" ] && echo "NOTHUMB := -marm" >> config.mak
+
+echo "EXTRA_CFLAGS= -g ${EXTRA_CFLAGS}" >> config.mak
+echo "EXTRA_LDFLAGS= ${EXTRA_LDFLAGS}" >> config.mak
+
+make fetch
+checkfail "contribs: make fetch failed"
+
+# gettext
+which autopoint >/dev/null || make $MAKEFLAGS .gettext
+# export the PATH
+export PATH="$PATH:$PWD/../$TARGET_TUPLE/bin"
+# Make
+make $MAKEFLAGS
+checkfail "contribs: make failed"
+
+cd ../../
+
+###################
+# BUILD DIRECTORY #
+###################
+
+VLC_BUILD_DIR=build-tizen-${TARGET_TUPLE}
+mkdir -p $VLC_BUILD_DIR && cd $VLC_BUILD_DIR
+
+#############
+# CONFIGURE #
+#############
+
+if [ ! -e ./config.h -o "$RELEASE" = 1 ]; then
+CPPFLAGS="$CPPFLAGS" \
+CFLAGS="$CFLAGS ${EXTRA_CFLAGS}" \
+CXXFLAGS="$CFLAGS ${EXTRA_CXXFLAGS}" \
+LDFLAGS="$LDFLAGS" \
+CC="${CROSS_COMPILE}gcc -fPIC --sysroot=${SYSROOT}" \
+CXX="${CROSS_COMPILE}g++ -fPIC --sysroot=${SYSROOT} -D__cpp_static_assert=200410" \
+NM="${CROSS_COMPILE}nm" \
+STRIP="${CROSS_COMPILE}strip" \
+RANLIB="${CROSS_COMPILE}ranlib" \
+AR="${CROSS_COMPILE}ar" \
+PKG_CONFIG_LIBDIR=../contrib/$TARGET_TUPLE/lib/pkgconfig \
+sh ../configure --host=$TARGET_TUPLE --build=x86_64-unknown-linux \
+                ${EXTRA_PARAMS} ${VLC_CONFIGURE_ARGS} ${OPTS}
+checkfail "vlc: configure failed"
+fi
+
+############
+# BUILDING #
+############
+
+echo -e "\e[1m\e[32mBuilding libvlc\e[0m"
+make $MAKEFLAGS
+checkfail "vlc: make failed"
+
+cd ../../
+
+##################
+# libVLC modules #
+##################
+
+REDEFINED_VLC_MODULES_DIR=${PROJECTPATH}/vlc/.modules/${VLC_BUILD_DIR}
+rm -rf ${REDEFINED_VLC_MODULES_DIR}
+mkdir -p ${REDEFINED_VLC_MODULES_DIR}
+
+echo -e "\e[1m\e[32mGenerating static module list\e[0m"
+blacklist_regexp=
+for i in ${VLC_MODULE_BLACKLIST}
+do
+    if [ -z "${blacklist_regexp}" ]
+    then
+        blacklist_regexp="${i}"
+    else
+        blacklist_regexp="${blacklist_regexp}|${i}"
+    fi
+done
+
+find_modules()
+{
+    echo "`find $1 -name 'lib*plugin.a' | grep -vE "lib(${blacklist_regexp})_plugin.a" | tr '\n' ' '`"
+}
+
+get_symbol()
+{
+    echo "$1" | grep vlc_entry_$2|cut -d" " -f 3
+}
+
+VLC_MODULES=$(find_modules vlc/$VLC_BUILD_DIR/modules)
+DEFINITION="";
+BUILTINS="const void *vlc_static_modules[] = {\n";
+for file in $VLC_MODULES; do
+    outfile=${REDEFINED_VLC_MODULES_DIR}/`basename $file`
+    name=`echo $file | sed 's/.*\.libs\/lib//' | sed 's/_plugin\.a//'`;
+    symbols=$("${CROSS_COMPILE}nm" -g $file)
+
+    # assure that all modules have differents symbol names
+    entry=$(get_symbol "$symbols" _)
+    copyright=$(get_symbol "$symbols" copyright)
+    license=$(get_symbol "$symbols" license)
+    cat <<EOF > ${REDEFINED_VLC_MODULES_DIR}/syms
+AccessOpen AccessOpen__$name
+AccessClose AccessClose__$name
+StreamOpen StreamOpen__$name
+StreamClose StreamClose__$name
+DemuxOpen DemuxOpen__$name
+DemuxClose DemuxClose__$name
+OpenFilter OpenFilter__$name
+CloseFilter CloseFilter__$name
+Open Open__$name
+Close Close__$name
+$entry vlc_entry__$name
+$copyright vlc_entry_copyright__$name
+$license vlc_entry_license__$name
+EOF
+    ${CROSS_COMPILE}objcopy --redefine-syms ${REDEFINED_VLC_MODULES_DIR}/syms $file $outfile
+    checkfail "objcopy failed"
+
+    DEFINITION=$DEFINITION"int vlc_entry__$name (int (*)(void *, void *, int, ...), void *);\n";
+    BUILTINS="$BUILTINS vlc_entry__$name,\n";
+done;
+BUILTINS="$BUILTINS 0\n};\n"; \
+printf "/* Autogenerated from the list of modules */\n$DEFINITION\n$BUILTINS\n" > ${PROJECTPATH}/vlc/.modules/libvlc-modules.c
+rm ${REDEFINED_VLC_MODULES_DIR}/syms
+
+##################
+#  Linking VLC   #
+##################
+
+echo -e "\e[1m\e[32mLinking\e[0m"
+
+${CC} -fPIC -rdynamic -shared \
+    -Lvlc/contrib/arm-linux-gnueabi/lib \
+    -o ${PROJECTPATH}/lib/libvlc.so \
+    ${PROJECTPATH}/vlc/.modules/libvlc-modules.c \
+    -Wl,--whole-archive \
+    ${PROJECTPATH}/vlc/build-tizen-arm-linux-gnueabi/lib/.libs/libvlc.a \
+    -Wl,--no-whole-archive \
+    ${PROJECTPATH}/vlc/.modules/build-tizen-arm-linux-gnueabi/*.a \
+    ${PROJECTPATH}/vlc/build-tizen-arm-linux-gnueabi/src/.libs/libvlccore.a \
+    ${PROJECTPATH}/vlc/build-tizen-arm-linux-gnueabi/compat/.libs/libcompat.a \
+    -Wl,--whole-archive \
+    ${PROJECTPATH}/vlc/contrib/arm-linux-gnueabi/lib/libiconv.a \
+    -Wl,--no-whole-archive \
+    -ldl -lz -lm \
+    -ldvbpsi -lmatroska -lebml -ltag \
+    -logg -lFLAC -ltheora -lvorbis \
+    -lmpeg2 -la52 \
+    -lavformat -lavcodec -lswscale -lavutil -lpostproc -lgsm -lopenjpeg \
+    -lliveMedia -lUsageEnvironment -lBasicUsageEnvironment -lgroupsock \
+    -lspeex -lspeexdsp \
+    -lxml2 -lpng -lgnutls -lgcrypt -lgpg-error \
+    -lnettle -lhogweed -lgmp \
+    -lfreetype -liconv -lass -lfribidi -lopus \
+    -ljpeg \
+    -ldvdnav -ldvdread -ldvdcss \
+    -ltasn1 \
+    -lmad \
+    -lzvbi \
+    -lssh2 \
+    -lmodplug \
+    -lupnp -lthreadutil -lixml \
+    -larchive \
+    ${EXTRA_LDFLAGS}
+
+# Missing:f
+# -lEGL -lGLESv2 -ldsm
+
+checkfail "linker: libvlc.so"
+
+if [ "$RELEASE" = 1 ]; then
+	echo -e "\e[1m\e[32mStripping\e[0m"
+    ${CROSS_COMPILE}strip ${PROJECTPATH}/lib/libvlc.so
+    checkfail "stripping"
+fi
+
+echo -e "\e[1m\e[32mDone! You can now build the Tizen application.\e[0m"
