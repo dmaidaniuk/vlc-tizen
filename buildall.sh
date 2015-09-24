69d705228dd5612512912516b6af9f6d097f5ceb
Thomas Guillem <thomas@gllm.fr>
2015-09-24 15:35:34 +0200

    build: factorize and fix x86 build

--- buildall.sh
+++ buildall.sh
@@ -4,10 +4,10 @@ SCRIPT=$(readlink -f "$0")
 PROJECTPATH=$(dirname "$SCRIPT")
 source ${PROJECTPATH}/buildcommon
 
-${PROJECTPATH}/buildvlc.sh
+${PROJECTPATH}/buildvlc.sh $*
 checkfail "buildvlc.sh failed"
 
-${PROJECTPATH}/buildemotion.sh
+${PROJECTPATH}/buildemotion.sh $*
 checkfail "buildemotion.sh failed"
 
 echo -e "\e[1m\e[32mDone! You can now build the Tizen application.\e[0m"
