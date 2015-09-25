d21e4781de850108dec2570cd694b2075ffa210d
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2015-09-25 13:55:19 +0200

    buildall.sh: Build medialibrary

--- buildall.sh
+++ buildall.sh
@@ -10,6 +10,9 @@ checkfail "buildvlc.sh failed"
 ${PROJECTPATH}/buildemotion.sh $*
 checkfail "buildemotion.sh failed"
 
+${PROJECTPATH}/buildmedialibrary.sh $*
+checkfail "buildmedialibrary.sh failed"
+
 cp ${PROJECTPATH}/.cproject-${TIZEN_ABI} ${PROJECTPATH}/.cproject
 
 echo -e "\e[1m\e[32mDone! You can now build the Tizen application.\e[0m"
