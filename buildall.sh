8a0ca06209f332159b879f41d5c9fa595bbaf8c2
Thomas Guillem <thomas@gllm.fr>
2015-09-25 09:10:40 +0200

    build: copy .cproject according to TIZEN_ABI

--- buildall.sh
+++ buildall.sh
@@ -10,4 +10,6 @@ checkfail "buildvlc.sh failed"
 ${PROJECTPATH}/buildemotion.sh $*
 checkfail "buildemotion.sh failed"
 
+cp ${PROJECTPATH}/.cproject-${TIZEN_ABI} ${PROJECTPATH}/.cproject
+
 echo -e "\e[1m\e[32mDone! You can now build the Tizen application.\e[0m"
