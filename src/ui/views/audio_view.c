635ed6fd52bb46b1095b6666aef0f97dbe6bb0ba
Jean-Baptiste Kempf <jb@videolan.org>
2015-09-30 17:28:08 +0200

    AudioView: rename an icon function to be more meaningfull

--- src/ui/views/audio_view.c
+++ src/ui/views/audio_view.c
@@ -103,7 +103,7 @@ free_list_item_data(void *data, Evas_Object *obj, void *event_info)
 }
 
 static Evas_Object*
-create_icon(Evas_Object *parent)
+create_icon_unknown(Evas_Object *parent)
 {
     return create_image(parent, "background_cone.png" );
 }
@@ -148,7 +148,7 @@ genlist_content_get_cb(void *data, Evas_Object *obj, const char *part)
         if (part && !strcmp(part, "elm.icon.1")) {
             content = elm_layout_add(obj);
             elm_layout_theme_set(content, "layout", "list/B/type.1", "default");
-            Evas_Object *icon = create_icon(content);
+            Evas_Object *icon = create_icon_unknown(content);
             elm_layout_content_set(content, "elm.swallow.content", icon);
         }
     }
