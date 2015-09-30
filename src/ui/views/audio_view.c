c64f6215e82b1ba138ac8c8844ff0f999d2183fc
Jean-Baptiste Kempf <jb@videolan.org>
2015-09-30 15:04:11 +0200

    Audio View: cleaning and split listing and item creation

--- src/ui/views/audio_view.c
+++ src/ui/views/audio_view.c
@@ -27,10 +27,6 @@
 #include "common.h"
 #include "system_storage.h"
 
-#include <Elementary.h>
-#include <storage.h>
-#include <app.h>
-
 #include "ui/interface.h"
 #include "ui/views/audio_view.h"
 #include "ui/audio_player.h"
@@ -39,44 +35,45 @@
 
 #include "media/media_item.h"
 
+#include <Elementary.h>
+
 typedef struct audio_view
 {
     interface *p_intf;
-    Evas_Object *nf_toolbar;
 
+    Evas_Object *nf_toolbar;
 } audio_view;
 
 typedef struct audio_list_item
 {
     audio_view *p_av;
 
-    media_item *mitem;
-
     const Elm_Genlist_Item_Class *itc;
 
+    media_item *p_media_item;
 } audio_list_item;
 
 static Evas_Object*
 create_audio_list(const char* path, audio_view *av);
 
 static void
-audio_gl_selected_cb(void *data, Evas_Object *obj EINA_UNUSED, void *event_info)
+genlist_selected_cb(void *data, Evas_Object *obj EINA_UNUSED, void *event_info)
 {
     audio_list_item *ald = data;
     struct stat sb;
-    stat(ald->mitem->psz_path, &sb);
+    stat(ald->p_media_item->psz_path, &sb);
 
     if (S_ISREG(sb.st_mode))
     {
         /* Launch the media player */
-        create_base_player(intf_get_mini_player(ald->p_av->p_intf), ald->mitem->psz_path);
+        create_base_player(intf_get_mini_player(ald->p_av->p_intf), ald->p_media_item->psz_path);
         LOGI("VLC Player launch");
     }
 
     else if (S_ISDIR(sb.st_mode))
     {
         /* Continue to browse media folder */
-        create_audio_list(ald->mitem->psz_path, ald->p_av);
+        create_audio_list(ald->p_media_item->psz_path, ald->p_av);
     }
     else
     {
@@ -89,11 +86,8 @@ static void
 free_list_item_data(void *data, Evas_Object *obj, void *event_info)
 {
     audio_list_item *ald = data;
-    /* Free the file path when the current genlist is deleted */
-    /* For example when the player is launched or a new genlist is created */
-    free(ald->mitem->psz_path);
-    LOGD("Path free");
-
+    free(ald->p_media_item->psz_path);
+    free(ald);
 }
 
 static Evas_Object*
@@ -103,7 +97,7 @@ create_icon(Evas_Object *parent)
 }
 
 static char *
-gl_text_get_cb(void *data, Evas_Object *obj, const char *part)
+genlist_text_get_cb(void *data, Evas_Object *obj, const char *part)
 {
     audio_list_item *ald = data;
     const Elm_Genlist_Item_Class *itc = ald->itc;
@@ -113,7 +107,7 @@ gl_text_get_cb(void *data, Evas_Object *obj, const char *part)
     /* Then put this string as the genlist item label */
     if (itc->item_style && !strcmp(itc->item_style, "2line.top.3")) {
         if (part && !strcmp(part, "elm.text.main.left.top")) {
-            asprintf(&buf, "<b>%s</b>", ald->mitem->psz_title);
+            asprintf(&buf, "<b>%s</b>", ald->p_media_item->psz_title);
             return buf;
         }
         else if (!strcmp(part, "elm.text.sub.left.bottom")) {
@@ -130,7 +124,7 @@ gl_text_get_cb(void *data, Evas_Object *obj, const char *part)
 }
 
 static Evas_Object*
-gl_content_get_cb(void *data, Evas_Object *obj, const char *part)
+genlist_content_get_cb(void *data, Evas_Object *obj, const char *part)
 {
     audio_list_item *ald = data;
     const Elm_Genlist_Item_Class *itc = ald->itc;
@@ -150,24 +144,45 @@ gl_content_get_cb(void *data, Evas_Object *obj, const char *part)
     return content;
 }
 
+audio_list_item *
+genlist_audio_item_create(audio_view *av, Evas_Object *genlist, char *psz_path, char *psz_title, Elm_Genlist_Item_Class *itc)
+{
+    audio_list_item *ald = malloc(sizeof(*ald));
+    ald->p_av = av;
+
+    ald->p_media_item = media_item_create(psz_path, MEDIA_ITEM_TYPE_AUDIO);
+    ald->p_media_item->psz_title = psz_title;
+
+    /* Set and append new item in the genlist */
+    Elm_Object_Item *it = elm_genlist_item_append(genlist,
+            itc,                            /* genlist item class               */
+            ald,                            /* genlist item class user data     */
+            NULL,                           /* genlist parent item              */
+            ELM_GENLIST_ITEM_NONE,          /* genlist item type                */
+            genlist_selected_cb,            /* genlist select smart callback    */
+            ald);                           /* genlist smart callback user data */
+
+    /* Put genlist item in the audio_list_data struct for callbacks */
+    ald->itc = itc;
+    /* */
+    elm_object_item_del_cb_set(it, free_list_item_data);
+    return ald;
+}
+
 Evas_Object*
 create_audio_list(const char* path, audio_view *av)
 {
-    char *buff;
-    audio_list_item *ald = malloc(sizeof(*ald));
-    ald->p_av = av;
-    const char *str = NULL;
+    char *str = NULL;
     DIR* rep = NULL;
     struct dirent* current_folder = NULL;
 
     /* Set then create the Genlist object */
     Evas_Object *parent = av->nf_toolbar;
     Evas_Object *genlist;
-    Elm_Object_Item *it;
     Elm_Genlist_Item_Class *itc = elm_genlist_item_class_new();
     itc->item_style = "2line.top.3";
-    itc->func.text_get = gl_text_get_cb;
-    itc->func.content_get = gl_content_get_cb;
+    itc->func.text_get = genlist_text_get_cb;
+    itc->func.content_get = genlist_content_get_cb;
     genlist = elm_genlist_add(parent);
 
     /* Set the genlist scoller mode */
@@ -178,7 +193,7 @@ create_audio_list(const char* path, audio_view *av)
     elm_genlist_mode_set(genlist, ELM_LIST_COMPRESS);
 
     /* Make a realpath to use a clean path in the function */
-    buff = realpath(path, NULL);
+    char *buff = realpath(path, NULL);
     path = buff;
 
     /* Open the path repository then put it as a dirent variable */
@@ -202,18 +217,9 @@ create_audio_list(const char* path, audio_view *av)
     /* Stop when the readdir have red the all repository */
     while ((current_folder = readdir(rep)) != NULL)
     {
-        audio_list_item *ald = malloc(sizeof(*ald));
-        ald->p_av = av;
-
         char *psz_path;
         /* Concatenate the file path and the selected folder or file name */
         asprintf(&psz_path, "%s/%s", path, current_folder->d_name);
-        ald->mitem = media_item_create(psz_path, MEDIA_ITEM_TYPE_AUDIO);
-
-        /* Put the folder or file name as a usable string for genlist item label */
-        str = current_folder->d_name;
-        /* Put the folder or file name in the audio_list_data struct for callbacks */
-        ald->mitem->psz_title = str;
 
         /* Avoid genlist item append for "." and ".." d_name */
         if (str && (strcmp(str, ".")==0 || strcmp(str, "..")==0))
@@ -221,19 +227,7 @@ create_audio_list(const char* path, audio_view *av)
             continue;
         }
 
-        /* Set and append new item in the genlist */
-        it = elm_genlist_item_append(genlist,
-                itc,                            /* genlist item class               */
-                ald,                            /* genlist item class user data     */
-                NULL,                            /* genlist parent item              */
-                ELM_GENLIST_ITEM_NONE,            /* genlist item type                */
-                audio_gl_selected_cb,            /* genlist select smart callback    */
-                ald);                            /* genlist smart callback user data */
-
-        /* Put genlist item in the audio_list_data struct for callbacks */
-        ald->itc = itc;
-        /* */
-        elm_object_item_del_cb_set(it, free_list_item_data);
+        genlist_audio_item_create(av, genlist, psz_path, str, itc);
     }
     /* */
     elm_genlist_item_class_free(itc);
@@ -324,7 +318,7 @@ create_audio_view(interface *intf, Evas_Object *parent)
     nf_it = elm_naviframe_item_push(av->nf_toolbar, NULL, NULL, NULL, NULL, "tabbar/icon/notitle");
 
     /* Push on the parent view */
-    elm_naviframe_item_push(parent, _("<b>Audio</b>"), NULL, NULL, av->nf_toolbar, "basic");
+    elm_naviframe_item_push(parent, "<b>Audio</b>", NULL, NULL, av->nf_toolbar, "basic");
 
     /* Create the toolbar in the view */
     tabbar = create_toolbar(av);
