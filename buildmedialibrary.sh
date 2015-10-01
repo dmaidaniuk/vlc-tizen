fa898f179841dcaea5c3e59a397092099eabb8ae
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2015-10-01 11:31:28 +0200

    buildmedialibrary.sh: Disable auto update

--- buildmedialibrary.sh
+++ buildmedialibrary.sh
@@ -12,8 +12,8 @@ if [ ! -d "${PROJECTPATH}/medialibrary" ]; then
     echo -e "\e[1m\e[32mmedialibrary source not found, cloning\e[0m"
     git clone http://github.com/chouquette/medialibrary.git "${PROJECTPATH}/medialibrary"
     checkfail "medialibrary source: git clone failed"
-else
-    ( cd ${PROJECTPATH}/medialibrary && git pull --rebase ) || echo "Failed to update medialibrary"
+#else
+#    ( cd ${PROJECTPATH}/medialibrary && git pull --rebase ) || echo "Failed to update medialibrary"
 fi
 
 if [ ! -d "${PROJECTPATH}/libvlcpp" ]; then
