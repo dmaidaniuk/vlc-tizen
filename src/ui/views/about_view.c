e1d89a120cceba77b6b09d88b500547247948bfa
Jean-Baptiste Kempf <jb@videolan.org>
2015-10-01 10:49:24 +0200

    Rewrite the About View

--- src/ui/views/about_view.c
+++ src/ui/views/about_view.c
@@ -32,30 +32,30 @@
 #include <Elementary.h>
 
 static Evas_Object*
-create_about_section(Evas_Object *nf_toolbar)
+create_about_section(Evas_Object *parent)
 {
-    // Add set set the box in the toolbar naviframe
-    Evas_Object *box = elm_box_add(nf_toolbar);
+    /* */
+    Evas_Object *box = elm_box_add(parent);
     elm_box_horizontal_set(box, EINA_FALSE);
     evas_object_size_hint_align_set(box, EVAS_HINT_FILL, EVAS_HINT_FILL);
 
-    //Add and set the label in the box
+    /* Add and set the label in the box */
     Evas_Object *lbl_about_title = elm_label_add(box);
     evas_object_size_hint_align_set(lbl_about_title, EVAS_HINT_FILL, 0.5);
     elm_object_text_set(lbl_about_title, "<align=center><b>Building VLC for Tizen</b></align>");
     elm_box_pack_end(box, lbl_about_title);
-
-    /* */
     evas_object_show(lbl_about_title);
 
+    /* */
+    evas_object_show(box);
     return box;
 }
 
 static Evas_Object*
-create_licence_section(Evas_Object *nf_toolbar)
+create_licence_section(Evas_Object *parent)
 {
     /* Add set set the box in the toolbar naviframe */
-    Evas_Object *box = elm_box_add(nf_toolbar);
+    Evas_Object *box = elm_box_add(parent);
     elm_box_horizontal_set(box, EINA_FALSE);
     evas_object_size_hint_align_set(box, EVAS_HINT_FILL, EVAS_HINT_FILL);
 
@@ -64,40 +64,31 @@ create_licence_section(Evas_Object *nf_toolbar)
     evas_object_size_hint_align_set(lbl_license_title, EVAS_HINT_FILL, 0.5);
     elm_object_text_set(lbl_license_title, "<align=center><b>VLC License for Tizen</b></align>");
     elm_box_pack_end(box, lbl_license_title);  //Add the label in the box
-
-    /* */
     evas_object_show(lbl_license_title);
 
+    /* */
+    evas_object_show(box);
     return box;
-}
-
-static void
-tabbar_item_selected(Evas_Object *nf_toolbar, Elm_Object_Item *about_it)
-{
-    const char *str = NULL;
-    Evas_Object *current_tab;
-    /* Get the item selected in the toolbar */
-    str = elm_object_item_text_get(about_it);
-
-    /* Create the view depending on the item selected in the toolbar */
-    if (str && !strcmp(str, "License")) {
-        current_tab = create_licence_section(nf_toolbar);
-    }
-    else     {
-        current_tab = create_about_section(nf_toolbar);
-    }
-
-    elm_object_content_set(nf_toolbar, current_tab ); //Add the created view in the toolbar naviframe
 }
 
 static void
 tabbar_item_cb(void *data, Evas_Object *obj, void *event_info)
 {
     Evas_Object *nf_toolbar = data;
-    Elm_Object_Item *about_it = event_info;
+    const char *str = elm_object_item_text_get((Elm_Object_Item *)event_info);
 
-    /* Call the function that creates the views */
-    tabbar_item_selected(nf_toolbar, about_it);
+    /* Create the view depending on the item selected in the toolbar */
+    Evas_Object *content;
+    if (str && !strcmp(str, "License")) {
+        content = create_licence_section(nf_toolbar);
+    }
+    else {
+        content = create_about_section(nf_toolbar);
+    }
+
+    /* Show it without title */
+    Elm_Object_Item *item = elm_naviframe_item_push(nf_toolbar, str, NULL, NULL, content, NULL);
+    elm_naviframe_item_title_enabled_set(item, EINA_FALSE, EINA_FALSE);
 }
 
 static Evas_Object*
@@ -105,20 +96,15 @@ create_toolbar(Evas_Object *nf_toolbar)
 {
     /* Create and set the toolbar */
     Evas_Object *tabbar = elm_toolbar_add(nf_toolbar);
-    elm_object_style_set(tabbar, "tabbar");
 
-    /* Set the toolbar shrink mode to NONE */
+    /* Set the toolbar options */
     elm_toolbar_shrink_mode_set(tabbar, ELM_TOOLBAR_SHRINK_NONE);
-    /* Expand the content to fill the toolbar */
     elm_toolbar_transverse_expanded_set(tabbar, EINA_TRUE);
-    /* Items will only call their selection func and callback when first becoming selected*/
     elm_toolbar_select_mode_set(tabbar, ELM_OBJECT_SELECT_MODE_DEFAULT);
 
     /* Append new entry in the toolbar with the Icon & Label wanted */
-    elm_toolbar_item_append(tabbar, ICON_DIR "ic_menu_goto.png",
-            "About", tabbar_item_cb, nf_toolbar);
-    elm_toolbar_item_append(tabbar, ICON_DIR "ic_menu_cone.png",
-            "License", tabbar_item_cb, nf_toolbar);
+    elm_toolbar_item_append(tabbar, NULL, "About",   tabbar_item_cb, nf_toolbar);
+    elm_toolbar_item_append(tabbar, NULL, "License", tabbar_item_cb, nf_toolbar);
 
     return tabbar;
 }
@@ -126,27 +112,29 @@ create_toolbar(Evas_Object *nf_toolbar)
 Evas_Object *
 create_about_view(Evas_Object *parent)
 {
-    Elm_Object_Item *nf_it, *tabbar_it;
-    Evas_Object *nf_toolbar,*tabbar;
+    /* Content box */
+    Evas_Object *about_box = elm_box_add(parent);
+    evas_object_size_hint_weight_set(about_box, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+    evas_object_size_hint_align_set(about_box, EVAS_HINT_FILL, EVAS_HINT_FILL);
 
     /* Toolbar Naviframe */
-    nf_toolbar = elm_naviframe_add(parent);
+    Evas_Object *nf_toolbar = elm_naviframe_add(about_box);
     evas_object_size_hint_weight_set(nf_toolbar, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
     evas_object_size_hint_align_set(nf_toolbar, EVAS_HINT_FILL, EVAS_HINT_FILL);
 
-    /* Toolbar Naviframe Settings */
-    elm_object_part_content_set(parent, "elm.swallow.content", nf_toolbar);
-//    nf_it = elm_naviframe_item_push(nf_toolbar, NULL, NULL, NULL, NULL, "tabbar/icon/notitle");
-//    elm_naviframe_item_push(parent, "<b>About</b>", NULL, NULL, nf_toolbar, "basic");
+    /* Create the toolbar in the view */
+    Evas_Object *tabbar = create_toolbar(nf_toolbar);
+
+    /* Add them to the box */
+    elm_box_pack_end(about_box, tabbar);
+    evas_object_show(tabbar);
+    elm_box_pack_end(about_box, nf_toolbar);
     evas_object_show(nf_toolbar);
 
-    /* Create the toolbar in the view */
-    tabbar = create_toolbar(nf_toolbar);
-    elm_object_item_part_content_set(nf_it, "tabbar", tabbar);
-
     /* Set the first item in the toolbar */
-    tabbar_it = elm_toolbar_first_item_get(tabbar);
-    elm_toolbar_item_selected_set(tabbar_it, EINA_TRUE);
+    elm_toolbar_item_selected_set(elm_toolbar_first_item_get(tabbar), EINA_TRUE);
 
-    return nf_toolbar;
+    /*  */
+    evas_object_show(about_box);
+    return about_box;
 }
