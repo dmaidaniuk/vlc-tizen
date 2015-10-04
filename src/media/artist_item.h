dace31f9c55b031d6aa90516b9c468661aab7a4d
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2015-10-04 13:04:03 +0200

    Add a basic artist_item type

---
+++ src/media/artist_item.h
@@ -0,0 +1,48 @@
+/*****************************************************************************
+ * Copyright © 2015 VideoLAN, VideoLabs SAS
+ *****************************************************************************
+ *
+ * Authors: Hugo Beauzée-Luyssen <hugo@beauzee.fr>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
+/*
+ * By committing to this project, you allow VideoLAN and VideoLabs to relicense
+ * the code to a different OSI approved license, in case it is required for
+ * compatibility with the Store
+ *****************************************************************************/
+
+#ifndef ARTIST_ITEM_H_
+# define ARTIST_ITEM_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct artist_item {
+    char* psz_name;
+} artist_item;
+
+artist_item*
+artist_item_create(const char* psz_name);
+
+void
+artist_item_destroy(artist_item* p_item);
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+#endif //ARTIST_ITEM_H_
