1ebf04ffbbeb3d85e86a1721d6547579b666a5a8
Ludovic Fauvet <etix@videolan.org>
2015-10-05 14:40:04 +0200

    settings: replace remnant variable causing issue with icons

--- src/ui/views/settings_view.c
+++ src/ui/views/settings_view.c
@@ -270,7 +270,7 @@ gl_content_get_cb(void *data, Evas_Object *obj, const char *part)
         if (part && !strcmp(part, "elm.icon.1")) {
             content = elm_layout_add(obj);
             elm_layout_theme_set(content, "layout", "list/B/type.3", "default");
-            Evas_Object *icon = create_icon(content, settings_menu[sd->index].icon);
+            Evas_Object *icon = create_icon(content, sd->menu[sd->index].icon);
             elm_layout_content_set(content, "elm.swallow.content", icon);
         }
         if (sd->menu[sd->index].type == SETTINGS_TYPE_TOGGLE)
