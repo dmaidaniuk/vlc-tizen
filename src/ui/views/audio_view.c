705561cc0588cd5fcb3d4bd17be207e205c207ca
Ludovic Fauvet <etix@videolan.org>
2015-10-21 15:38:27 +0200

    audio view: close overflow menu if already open

--- src/ui/views/audio_view.c
+++ src/ui/views/audio_view.c
@@ -177,8 +177,16 @@ audio_view_callback(view_sys *p_view_sys, interface_view_event event)
     switch (event) {
     case INTERFACE_VIEW_EVENT_MENU:
     {
-        p_view_sys->p_overflow_menu = popup_menu_add(audio_view_popup_menu, p_view_sys, p_view_sys->nf_toolbar);
-        evas_object_show(p_view_sys->p_overflow_menu);
+        if (p_view_sys->p_overflow_menu)
+        {
+            evas_object_del(p_view_sys->p_overflow_menu);
+            p_view_sys->p_overflow_menu = NULL;
+        }
+        else
+        {
+            p_view_sys->p_overflow_menu = popup_menu_add(audio_view_popup_menu, p_view_sys, p_view_sys->nf_toolbar);
+            evas_object_show(p_view_sys->p_overflow_menu);
+        }
         return true;
     }
     case INTERFACE_VIEW_EVENT_BACK:
