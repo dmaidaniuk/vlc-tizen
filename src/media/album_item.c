d633e02e357f67f9768d9d4ad2ea746cc4aae2cc
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2015-10-23 15:15:42 +0200

    album_item: Fix copy function

--- src/media/album_item.c
+++ src/media/album_item.c
@@ -53,6 +53,7 @@ album_item_copy(const album_item* p_item)
         p_new_item->psz_summary = strdup(p_item->psz_summary);
     if (p_item->psz_artwork != NULL)
         p_new_item->psz_artwork = strdup(p_item->psz_artwork);
+    p_new_item->i_release_date = p_item->i_release_date;
     return p_new_item;
 }
 
