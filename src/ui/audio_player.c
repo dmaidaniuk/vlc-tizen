35048a4f0fcc6f7250bbc15b4f20bb2c3bc3cc47
Ludovic Fauvet <etix@videolan.org>
2015-11-12 13:21:03 +0100

    audio player: reset the state of the 'more' button on back press too

--- src/ui/audio_player.c
+++ src/ui/audio_player.c
@@ -327,6 +327,8 @@ audio_player_handle_back_key(audio_player *mpd)
 {
     if (mpd->popup)
     {
+        mpd->more_state = false;
+        elm_image_file_set(mpd->fs_more_btn, ICON_DIR"ic_more_circle_normal_o.png", NULL);
         evas_object_del(mpd->popup);
         return true;
     }
