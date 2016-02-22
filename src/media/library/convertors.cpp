ddd444ac7d67cc7dab96d1718ec14d4ff0cf8d80
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2016-02-22 12:10:38 +0100

    ml: releaseDate is now part of IMedia

--- src/media/library/convertors.cpp
+++ src/media/library/convertors.cpp
@@ -124,7 +124,7 @@ fileToMediaItem( MediaPtr media )
             if (album != nullptr)
             {
                 media_item_set_meta(mi, MEDIA_ITEM_META_ALBUM, album->title().c_str());
-                auto year = albumTrack->releaseYear();
+                auto year = media->releaseDate();
                 if (year != 0)
                 {
                     media_item_set_meta(mi, MEDIA_ITEM_META_YEAR, std::to_string(year).c_str());
